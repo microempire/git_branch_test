35b2374f3975c06ae296ef87557638391b0c5ed2
Than Zaw Toe office <microempire37@gmail.com>
2022-12-16 10:43:02 +0630

    free version two

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main ()
     std::cout<< "This is branch Testing" << std::endl;
     std::cout << "This is made for the second version "<< std:: endl;
     std::cout << "This is made for the free version one" << std:: endl;
+    std::cout << "This is made for the free version two" << std:: endl;
+    
     
     return 0;
 }
