7dba8b065b9a9d3dfc89813446a11af06a63f54d
Than Zaw Toe office <microempire37@gmail.com>
2022-12-16 12:13:54 +0630

    factorial version add

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+int factorial (int num);
 
 int main ()
 {
     std::cout<< "Hello World"<<std::endl;
     std::cout<< "This is branch Testing" << std::endl;
     
+    int num=0;
+
+    do{
+        std::cout << "Enter positive integer: " << std::endl;
+        cin>>num;
+
+    }
+    while(num<0);
+    int factorial;
+    for (int i=i;i<=num;i++)
+    {
+        factorial = factorial *i;
+    }
+    std::cout<< num << " !"
     return 0;
 }
