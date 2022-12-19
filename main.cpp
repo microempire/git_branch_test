884127d14b40b46c74e0778647dc6db2859ed8f1
Than Zaw Toe Home <microempire37@gmail.com>
2022-12-19 13:21:58 +0630

    new branch call Free add

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main ()
     std::cout<< "Hello World"<<std::endl;
     std::cout<< "This is branch Testing" << std::endl;
     std::cout << "This is made for the second version "<< std:: endl;
+    std::cout << "This is for free branch b1" << std:: endl;
     
-    return 0;
+        return 0;
 }
