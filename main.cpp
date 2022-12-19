7d3d4da93e858ae87c6d2efe2c5d54418f056ffd
Than Zaw Toe Home <microempire37@gmail.com>
2022-12-19 14:14:56 +0630

    paid version p1

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main ()
     std::cout << "This is made for the second version "<< std:: endl;
     std:: cout << "This is for v3 master branch" << std:: endl;
     std:: cout << "This is for v4 master branch" << std:: endl;
+    std:: cout << "This is for paid version p1" << std :: endl;
+    
     
     return 0;
 }
