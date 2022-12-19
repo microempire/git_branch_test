bd638ee12e1ace953adb6015483d12b99229b43b
Than Zaw Toe Home <microempire37@gmail.com>
2022-12-19 13:33:02 +0630

    free branch F2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main ()
     std::cout<< "Hello World"<<std::endl;
     std::cout<< "This is branch Testing" << std::endl;
     std::cout << "This is made for the second version "<< std:: endl;
-    std::cout << "This is for free branch b1" << std:: endl;
+    std::cout << "This is for free branch F1" << std:: endl;
+    std::cout << "This is for free branch F2" << std :: endl;
+
     
         return 0;
 }
